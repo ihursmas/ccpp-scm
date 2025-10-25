54634350dc496b7d9499246fa20cb6a215fbd69e
ihursmas <i-kuan.hu@noaa.gov>
2025-10-25 06:25:43 +0000

    For PUMAS to work appropriately

--- scm/src/CCPP_STATIC_API.sh
+++
@@ -1,6 +0,0 @@
-# The CCPP static API is defined here.
-#
-# This file is auto-generated using ccpp_prebuild.py
-# at compile time, do not edit manually.
-#
-export CCPP_STATIC_API="/scratch4/BMC/gsienkf/I-kuan.Hu/pumas.prtest/scm/src/ccpp_static_api.F90"
